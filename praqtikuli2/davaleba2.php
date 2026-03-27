5c280a04779a4e3a99e3d8bbda8591f1730bf692
Emil Kirakosyan_gau <emil.kirakosiani@gau.edu.ge>
2026-03-27 22:55:34 +0400

    Add new PHP file to generate and display a random 4x4 table

---
+++ praqtikuli2/davaleba2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php 
+    $arr = [];
+    for ($i=0; $i < 4; $i++) { 
+        for ($j=0; $j < 4; $j++) { 
+            $arr[$i][$j] = rand(10, 100);
+        }
+    }
+
+    printTable($arr);
+
+    function printTable($arr) {
+        echo "<table border='1'>";
+        for ($i=0; $i < count($arr); $i++) { 
+            echo "<tr>";
+            for ($j=0; $j < count($arr[0]); $j++) { 
+                echo "<td>" . $arr[$i][$j] . "</td>";
+            }
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
+?>
+<body>
+    
+</body>
+</html>
